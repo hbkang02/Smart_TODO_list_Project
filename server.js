c3192c373bb8cb6d914de78ed7345b7df7e2d578
RyanDeMesa <ryandemesa09@gmail.com>
2023-01-03 17:48:46 +0000

    Add style and format for home/login page

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const login = require('./routes/login');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -39,6 +40,7 @@ const usersRoutes = require('./routes/users');
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+app.use('/login', login);
 // Note: mount other resources here, using the same pattern above
 
 // Home page

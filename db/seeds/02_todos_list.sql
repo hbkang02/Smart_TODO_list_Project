6038eb8fef9a35ad3df2614167379a18747d5df1
John Kang <hbkang02@gmail.com>
2023-01-02 19:07:05 -0800

    added seeds into 02_todos_list.sql

---
+++ db/seeds/02_todos_list.sql
@@ -0,0 +1,17 @@
+INSERT INTO todos (category_id, user_id, todo_name, created_date, completed_date)
+VALUES (
+  1,
+  1,
+  'Read Harry Potter',
+  now(),
+  NULL
+)
+INSERT INTO todos (category_id, user_id, todo_name, created_date, completed_date)
+VALUES (
+  2,
+  1,
+  'Buy Apple watch',
+  now(),
+  NULL
+)
+

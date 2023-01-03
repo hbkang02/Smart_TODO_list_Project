e21ca14a711c0d59f1b55813a8d41b098fe8db66
John Kang <hbkang02@gmail.com>
2023-01-03 10:25:22 -0800

    database and login routes functional

--- server.js
+++ server.js
@@ -31,14 +31,17 @@ app.use(express.static('public'));
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
-const usersRoutes = require('./routes/users');
+const loginApiRoutes = require('./routes/login-api');
+// const usersRoutes = require('./routes/users');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
+
+app.use('/api/users-api', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
-app.use('/users', usersRoutes);
+app.use('/login', loginApiRoutes);
+// app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page

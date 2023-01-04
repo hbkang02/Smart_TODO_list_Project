f291054081ceb2ea7ebad0139415787279617419
RyanDeMesa <ryandemesa09@gmail.com>
2023-01-04 00:16:47 +0000

    Create get route/function to load lists

--- routes/todos.js
+++ routes/todos.js
@@ -1,2 +1,30 @@
 const express = require('express');
 const router = express.Router();
+
+const db = require('../db/connection');
+
+const getUsersToDos = function(userId) {
+  const text = `
+  SELECT * FROM todos
+  WHERE user_id = $1`;
+  const values = [userId];
+
+  return db.query(text, values)
+    .then(data => data.rows)
+    .catch(err => console.error(this, 'query failed', err.stack));
+};
+
+router.get("/", (req, res) => {
+  const userId = req.session.user_id;
+  if (!userId) {
+    res.send('Not logged in!');
+    return;
+  }
+
+  getUsersToDos(userId)
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ error: err.message }));
+
+});
+
+module.exports = router;

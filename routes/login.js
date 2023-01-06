2b3e1b478a7b598b37703670bc8fc0fbd57b2114
RyanDeMesa <ryandemesa09@gmail.com>
2023-01-06 04:37:26 +0000

    fix post for register form

--- routes/login.js
+++ routes/login.js
@@ -30,8 +30,6 @@ router.get('/', (req, res) => {
   res.render('login', templateVars);
 });
 
-module.exports = router;
-
 router.post('/', (req, res) => {
   const { email, password } = req.body;
   login(email, password)
--- routes/register.js
+++ routes/register.js
@@ -2,47 +2,45 @@
 registration routing
  */
 
-const { addUser } = require('../database')
-const express = require('express');
+const { addUser } = require("../database");
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const { getUserWithEmail } = require('../database')
+const bcrypt = require("bcrypt");
+const { getUserWithEmail } = require("../database");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   let templateVars = {
     userId: req.session.email,
   };
-  res.render('registration', templateVars);
+  res.render("registration", templateVars);
 });
 
 router.post("/", (req, res) => {
   const email = req.body.email;
   const password = bcrypt.hashSync(req.body.password, 12);
   const name = req.body.name;
-  if (email === '' || req.body.password === '') {
+  if (email === "" || req.body.password === "") {
     res.sendStatus(400);
   } else {
-    getUserWithEmail(email)
-      .then((user) => {
-        if (user) {
-          res.redirect("/");
-        } else {
-          let user = { name, email, password };
-          req.session.userId = user.id;
-          addUser(user)
-            .then(user => {
-              if (!user) {
-                res.send({ error: "error" });
-                return;
-              }
-              res.send("🤗");
-            })
-            .catch(e => res.send(e));
-        }
-      })
-
+    getUserWithEmail(email).then((user) => {
+      if (user) {
+        res.redirect("/login");
+      } else {
+        let user = { name, email, password };
+        addUser(user)
+          .then((user) => {
+            if (!user) {
+              res.send({ error: "error" });
+              return;
+            }
+            req.session.userId = user.id;
+            res.redirect("/");
+            console.log("redirected");
+          })
+          .catch((e) => res.send(e));
+      }
+    });
   }
-
 });
 
 module.exports = router;

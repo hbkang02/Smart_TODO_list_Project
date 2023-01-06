550ad3e2e22632f44209d5777248e9f851f41a3c
John Kang <hbkang02@gmail.com>
2023-01-06 03:32:46 -0800

    fixed bug and updated popup menu for edit

--- server.js
+++ server.js
@@ -63,15 +63,14 @@ app.use("/register", register);
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
-  console.log(req.session.userId)
   if (!req.session.userId) {
-    console.log("hi")
     return res.redirect("/register");
   }
   return getUserWithId(req.session.userId)
   .then ((user) => {
     // Get items
-    getTodo(req.session.userId).then(todoRes => {
+    getTodo(req.session.userId)
+    .then(todoRes => {
       const readItems = [];
       const watchItems = [];
       const buyItems = [];
@@ -108,7 +107,8 @@ app.get("/", (req, res) => {
         readItems,
         watchItems,
         buyItems,
-        eatItems
+        eatItems,
+        editItemId: req.session.todoId
       };
       res.render("index", templateVars);
     });
